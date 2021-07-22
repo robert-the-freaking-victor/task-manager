660cc6e3c1a8e5e10ecb810f35624b461379fb7f
borreti <36523422+borreti@users.noreply.github.com>
2021-07-21 23:20:33 -0300

    adding the models

---
+++ backend/src/api/models/TaskModel.ts
@@ -0,0 +1,56 @@
+import { Schema, ObjectId } from 'mongoose';
+import mongoose from '../database/mongoDB';
+
+
+const taskSchema = new Schema({
+	Title: String,
+	Description: String,
+	Status: String
+});
+
+export interface ITask {
+	Title?: string;
+	Description?: string;
+	Status?: string;
+}
+
+export default class TaskModel {
+
+	protected static Task = mongoose.model('Task', taskSchema);
+
+	public static async create(body: ITask) {
+		try {
+		const task = await TaskModel.Task.create(body);
+		return task;
+		} catch(err) {
+			console.log(err);
+		}
+	}
+
+	public static async list() {
+		try {
+			const tasks = await TaskModel.Task.find({});
+			return tasks;
+		} catch(err) {
+			console.log(err);
+		}
+	}
+
+	public static async get(id: string) {
+		try {
+			const task = await TaskModel.Task.findById(id);
+			return task;
+		} catch(err) {
+			console.log(err);
+		}
+	}
+
+	public static async update(id: ObjectId, body: ITask) {
+		try {
+			const task = await TaskModel.Task.findById(id);
+
+		} catch(err) {
+			console.log(err);
+		}
+	}
+}
