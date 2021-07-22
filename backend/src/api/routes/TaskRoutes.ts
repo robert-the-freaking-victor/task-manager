85a3ffa3587dadde5f29035bcba6188af91bfed7
borreti <36523422+borreti@users.noreply.github.com>
2021-07-21 23:49:16 -0300

    adding routes to list, update, delete

--- backend/src/api/routes/TaskRoutes.ts
+++ backend/src/api/routes/TaskRoutes.ts
@@ -6,7 +6,10 @@ export default class TaskRoutes {
 
 	public static async run() {
 		app.post('/Tasks', TaskController.create);
+		app.get('/Tasks', TaskController.list);
 		app.get('/Tasks/:id', TaskController.get);
+		app.put('/Tasks/:id', TaskController.update);
+		app.delete('/Tasks/:id', TaskController.delete);
 		return true;
 	}
 }
